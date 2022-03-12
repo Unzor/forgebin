60046db67528c119bb02f4c93e93a252056ea0a5
Unzor <71789103+Unzor@users.noreply.github.com>
2022-03-11 21:45:10 -0600

    Update octoveal.js

--- src/octoveal.js
+++ src/octoveal.js
@@ -1,45 +1,52 @@
 const fs = require("fs");
-const { Octokit, App } = require("octokit");
+const {
+    Octokit,
+    App
+} = require("octokit");
 class Octoveal {
-  constructor(settings) {
-    this.auth = settings.auth;
-    this.repo = settings.repo;
-    this.owner = settings.owner;
-    global.octokit = new Octokit({auth: this.auth});
-  }
-  async addAssetToRelease(tag, file){
-     var release = await octokit.rest.repos
-      .getReleaseByTag({
-        owner: this.owner,
-        repo: this.repo,
-        tag: tag,
-      });
-
-    console.log(file);
-	
-      await octokit.request({
-          method: "POST",
-          url: release,
-          headers: {
-            "content-type": "text/plain",
-          },
-          data: fs.readFileSync(file).toString(),
-          name: file,
-          label: file,
+    constructor(settings) {
+        this.auth = settings.auth;
+        this.repo = settings.repo;
+        this.owner = settings.owner;
+        global.octokit = new Octokit({
+            auth: this.auth
         });
-  }
-async createRelease(tag) {
-  return await octokit.request(`POST /repos/${this.owner}/${this.repo}/releases`, {
-  owner: this.owner,
-  repo: this.repo,
-  tag_name: tag
-});
-  }
-  async getLatestRelease(){
-    return await octokit.request(`GET /repos/${this.owner}/${this.repo}/releases/latest`, {
-  owner: this.owner,
-  repo: this.repo
-})
-  }
+    }
+    async addAssetToRelease(tag, file) {
+        var release = await octokit.rest.repos
+            .getReleaseByTag({
+                owner: this.owner,
+                repo: this.repo,
+                tag: tag,
+            });
+
+        console.log(file);
+
+        await octokit.request({
+            method: "POST",
+            url: release,
+            headers: {
+                "content-type": "text/plain",
+            },
+            data: fs.readFileSync(file).toString(),
+            name: file,
+            label: file,
+        });
+    }
+    async createRelease(tag) {
+        return await octokit.request(`POST /repos/${this.owner}/${this.repo}/releases`, {
+            owner: this.owner,
+            repo: this.repo,
+            tag_name: tag
+        });
+    }
+    async getLatestRelease() {
+        return await octokit.request(`GET /repos/${this.owner}/${this.repo}/releases/latest`, {
+            owner: this.owner,
+            repo: this.repo
+        })
+    }
 }
-module.exports = {Octoveal: Octoveal}
+module.exports = {
+    Octoveal: Octoveal
+}
